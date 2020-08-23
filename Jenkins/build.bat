97dcc6c9e51f443904a61b50bd5668a2f793b80f
Pedro <pedro@enclave.vn>
2020-08-23 12:01:37 +0700

    - update

--- Jenkins/build.bat
+++ Jenkins/build.bat
@@ -4,8 +4,9 @@ echo "****** Building the image ***************"
 echo "*****************************************"
 
 docker build -t demo-image -f Jenkins/Docker/Dockerfile --no-cache .
+docker rmi -f $(docker images -f "dangling=true" -q)
+docker rmi $(docker images|grep "<none>"|awk '$1=="<none>" {print $3}')
 
 echo "*****************************************"
-echo "****** Done build the image **************"
+echo "****** Done build the image *************"
 echo "*****************************************"
-echo Done.
