7453f6ab79e7b557d333ed5f134d2ab5e937298b
Pedro <pedro@enclave.vn>
2020-08-23 10:54:13 +0700

    - update docker build

--- Jenkins/build.bat
+++ Jenkins/build.bat
@@ -3,7 +3,7 @@ echo "*****************************************"
 echo "****** Building the image ***************"
 echo "*****************************************"
 
-docker build . -t demo-image -f Jenkins/Docker/Dockerfile
+docker build -t demo-image -f Jenkins/Docker/Dockerfile --no-cache .
 
 echo "*****************************************"
 echo "****** Done build the image **************"
