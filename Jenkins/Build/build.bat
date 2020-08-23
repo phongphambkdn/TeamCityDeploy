33fc166cdca9537a8a28bc27bbd556a33970119b
Pedro <pedro@enclave.vn>
2020-08-23 17:29:15 +0700

    - remove from build

--- Jenkins/Build/build.bat
+++ Jenkins/Build/build.bat
@@ -4,7 +4,6 @@ echo "****** Building the image ***************"
 echo "*****************************************"
 
 docker-compose -f Jenkins/Build/docker-compose.yml build --no-cache
-docker image prune -f
 
 echo "*****************************************"
 echo "****** Done build the image *************"
