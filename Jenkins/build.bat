22cd0e111a4574f39bbce7d3262de572550a4869
Pedro <pedro@enclave.vn>
2020-08-23 10:46:16 +0700

    - update window file

--- Jenkins/build.bat
+++ Jenkins/build.bat
@@ -1,9 +1,11 @@
+@ECHO OFF
 echo "*****************************************"
 echo "****** Building the image ***************"
 echo "*****************************************"
 
-docker build . -f Jenkins/Docker/Dockerfile
+docker build . -t demo-image -f Jenkins/Docker/Dockerfile
 
 echo "*****************************************"
 echo "****** Done build the image **************"
 echo "*****************************************"
+echo Done.
